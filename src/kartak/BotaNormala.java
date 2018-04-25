9bf4a512f8356fc0bfd50f2a5b8014e440acb4b0
Ander <alopezherrera97@gmail.com>
2018-04-25 19:16:23 +0200

    Krokodilo burututa

--- src/kartak/BotaNormala.java
+++ src/kartak/BotaNormala.java
@@ -10,6 +10,17 @@ public class BotaNormala implements Bota {
 		ListaKartakTableroan ik = Tablero.getTableroa().getJokoan();
 		ik.kenduAnimali(pPos);
 	}
+	public void tablerotikAtera(int pHasiera,int pZenbat) {
+		// TODO Auto-generated method stub
+		int aux1 = pHasiera;
+		int aux2 = pZenbat;
+		ListaKartakTableroan ik = Tablero.getTableroa().getJokoan();
+		while (aux2!=0) {
+			ik.kenduAnimali(aux1+aux2);
+			aux2--;
+		}
+			
+	}
 	
 	
 
--- src/kartak/Krokodilo.java
+++ src/kartak/Krokodilo.java
@@ -1,23 +1,43 @@
 package kartak;
+
+import kodea.KartaNodo;
+import kodea.ListaKartakTableroan;
+
 //Un cocodrilo se come a cualquier animal 
-//más débil 
+//mï¿½s dï¿½bil 
 //que tenga 
 //por delante. Si mientras 
-//come se topa con un animal más fuerte o con una cebra, el cocodrilo se detiene de inmediato.
-//Los animales comidos van a parar a la carta «ES LO QUE HAY
+//come se topa con un animal mï¿½s fuerte o con una cebra, el cocodrilo se detiene de inmediato.
+//Los animales comidos van a parar a la carta ï¿½ES LO QUE HAY
 //El cocodrilo realiza su animalada de forma recurrente en cada turno sucesivo de cada jugador.
 public class Krokodilo extends KartaAnimali{
-
+	private OrdenatuNormala 	ordenatu;
+	private BotaNormala 		bota;
 	public Krokodilo(String kolorea, int zenbakia) {
 		super(kolorea, zenbakia);
 		// TODO Auto-generated constructor stub
-		this.errekurrentea=true;
+		this.errekurrentea 	=	true;
+		this.ordenatu 		= 	new OrdenatuNormala();
+		this.bota			=	new BotaNormala();
 	}
 
 	@Override
 	public void animaladaBurutu() {
-		// TODO Auto-generated method stub
-		
+		ListaKartakTableroan tablero = ListaKartakTableroan.getNireListaKartakTableroan();
+		Boolean amaituta 	= 	false;
+		int		zenbat		= 	0;
+		int 		hasiera		=	tablero.posizioaLortu(this.path);
+		KartaNodo	aux		=	tablero.getKartaListan(hasiera);
+		aux.getAurrekoa();
+		while (amaituta==false) {
+			if ((aux.getKarta().animaliZenbakia()<10)&&(aux.getKarta().getIzena()!="Zebra")){
+				zenbat++;
+			}else {
+				amaituta=true;
+			}
+			aux=aux.getHurrengoa();
+		}
+		this.bota.tablerotikAtera(hasiera,zenbat);
 	}
 
 }
