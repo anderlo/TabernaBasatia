939b18ab20be2be9504dcd20ff5c524c19e786e4
Alain <gotazpi@gmail.com>
2018-04-25 19:32:40 +0200

    Hipopotamo, jirafa eta lehoia eginda

--- src/kartak/Hipopotamo.java
+++ src/kartak/Hipopotamo.java
@@ -1,5 +1,8 @@
 package kartak;
 
+import kodea.Jokoa;
+import kodea.ListaKartakTableroan;
+
 //Un hipopótamo embiste hacia la puerta. Sin embargo, no puede colarse delante de un 
 //miembro de su propia especie, de animales más fuertes (leones) o de las cebras (consultad 
 //«La cebra»).
@@ -7,16 +10,31 @@ package kartak;
 //El hipopótamo realiza su animalada de forma recurrente en cada turno sucesivo de 
 //cada jugador
 public class Hipopotamo extends KartaAnimali{
+	private Ordenatu ordenatu;
 
 	public Hipopotamo(String kolorea, int zenbakia) {
 		super(kolorea, zenbakia);
 		// TODO Auto-generated constructor stub
 		this.errekurrentea=true;
+		this.ordenatu = new OrdenatuNormala();
 	}
 
 	@Override
 	public void animaladaBurutu() {
 		// TODO Auto-generated method stub
+		ListaKartakTableroan kartak = Jokoa.getJokoa().getTablero().getJokoan();
+		int pos = kartak.posizioaLortu(path);
+		boolean amaitu = false;
+		for (int i=0; (i<pos)&&!(amaitu);i++){
+			if (kartak.getKartaListan(pos-1).getKarta().getIzena().equals("Hipopotamo")||
+				kartak.getKartaListan(pos-1).getKarta().getIzena().equals("Lehoi")||
+				kartak.getKartaListan(pos-1).getKarta().getIzena().equals("Zebra")){
+				amaitu = true;
+			}
+			else {
+				ordenatu.ordenatu(pos-i, 1);
+			}
+		}
 		
 	}
 
--- src/kartak/Lehoi.java
+++ src/kartak/Lehoi.java
@@ -1,5 +1,8 @@
 package kartak;
 
+import kodea.Jokoa;
+import kodea.ListaKartakTableroan;
+
 //Si el león jugado no se encuentra en la cola con un miembro de su especie, espanta a 
 //todos los monos hacia la carta «ES LO QUE HAY». Seguidamente, se cuela delante de 
 //todos los demás animales
@@ -10,19 +13,39 @@ package kartak;
 //«ES LO QUE HAY»
 
 public class Lehoi extends KartaAnimali{
+	private Ordenatu ordenatu;
+	private Bota bota;
 
 	public Lehoi(String kolorea, int zenbakia) {
 		
 		super(kolorea, zenbakia);
 		this.animalia="Lehoi";
 		this.setPath();
+		this.ordenatu = new OrdenatuNormala();
+		this.bota = new BotaNormala();
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void animaladaBurutu() {
 		// TODO Auto-generated method stub
-		
+		ListaKartakTableroan kartak = Jokoa.getJokoa().getTablero().getJokoan();
+		boolean lehoiBatDago = false;
+		for (int i=0;(i<kartak.luzera()-1)&&!(lehoiBatDago);i++ ) {
+			if (kartak.getKartaListan(i).getKarta().getIzena().equals("Lehoi")) {
+				lehoiBatDago=true;
+			}
+		}
+		if (lehoiBatDago){
+			for (int i=0;(i<kartak.luzera()-1);i++ ) {
+				if (kartak.getKartaListan(i).getKarta().getIzena().equals("Tximino")) {
+					bota.tablerotikAtera(i);
+				}
+			}
+		}
+		else {
+			bota.tablerotikAtera(kartak.posizioaLortu(path));
+		}
 	}
 
 }
